e6bdb2424cda723b4b5c63082b7f3821602a63ba
jundellagbo <jundellagbo10dd@gmail.com>
2025-10-31 18:24:52 +0800

    init

---
+++ function.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: MBN Oxygen Enhancer
+Plugin URI: https://github.com/MBNDEV/mbn-oxygen-enhancer
+Description: Enhances Oxygen Builder with performance optimizations and extra utilities.
+Version: 1.0.0
+Author: My Biz Niche
+Author URI: https://www.mybizniche.com/
+License: GPL2
+Text Domain: mbn-oxygen-enhancer
+*/
